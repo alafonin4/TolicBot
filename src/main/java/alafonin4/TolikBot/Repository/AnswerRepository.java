e6feb51145e3e03edfda63187b5fda853308a6cf
Afonin Alexei <aleksafonin2@mail.ru>
2024-08-21 18:00:40 +0300

    Update AnswerRepository.java

--- src/main/java/alafonin4/TolikBot/Repository/AnswerRepository.java
+++ src/main/java/alafonin4/TolikBot/Repository/AnswerRepository.java
@@ -2,9 +2,11 @@ package alafonin4.TolikBot.Repository;
 
 import alafonin4.TolikBot.Entity.Answer;
 import alafonin4.TolikBot.Entity.Image;
+import alafonin4.TolikBot.Entity.Question;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnswerRepository extends CrudRepository<Answer, Long> {
+    Answer findAllByQuestion(Question q);
 }
