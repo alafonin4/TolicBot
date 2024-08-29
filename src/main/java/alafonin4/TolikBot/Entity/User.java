7e9bfd3657e84aa0b123dc5d85d38c2ec5ccae4e
Afonin Alexei <aleksafonin2@mail.ru>
2024-08-29 16:24:37 +0300

    Update User.java

--- src/main/java/alafonin4/TolikBot/Entity/User.java
+++ src/main/java/alafonin4/TolikBot/Entity/User.java
@@ -22,7 +22,7 @@ public class User {
     private Role role;
 
     @Enumerated(value = EnumType.STRING)
-    @Column(name = "stage", nullable = false)
+    @Column(name = "stage")
     private Stage stage;
 
     @Column(name = "number_of_invited_users", nullable = false)
