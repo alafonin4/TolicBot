de19c9583e717acb5439815f8eef4bcfbf4df63a
Afonin Alexei <aleksafonin2@mail.ru>
2024-09-04 16:00:52 +0300

    Update OrderRepository.java

--- src/main/java/alafonin4/TolikBot/Repository/OrderRepository.java
+++ src/main/java/alafonin4/TolikBot/Repository/OrderRepository.java
@@ -2,6 +2,7 @@ package alafonin4.TolikBot.Repository;
 
 import alafonin4.TolikBot.Entity.Order;
 import alafonin4.TolikBot.Entity.Status;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +14,5 @@ public interface OrderRepository extends CrudRepository<Order, Long> {
     Optional<Order> findFirstByStatusOrderByCreatedAtDesc(Status status);
 
     List<Order> findAllByStatus(Status unseen);
+    List<Order> findAllByUserChatId(long chatId);
 }
